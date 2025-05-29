4175c51e5913e432ab129a6bf21e51ff888cebf7
IrfanTheDev <theirfan2020@gmail.com>
2025-05-29 13:23:39 +0530

    Fixed : Toast msg

--- src/constant.js
+++ src/constant.js
@@ -3,12 +3,12 @@ export default {
     // Variables to control the code and feature based on sapces,
     // So that even after merging code does not break or go into production without testing.
 
-    isLocal: true, //When running it locally.
+    isLocal: false, //When running it locally.
     isDev: false,  //For dev space only, also this should be true while deploying to Dev space
     isTest: true, // for test space only, this should be true while deploying to test space
     isProd: false, // for prod space only, and while deploying to the prod
 
-    version: "0.5.3",
-    lastBuild: "27-05-2025"
+    version: "0.5.4",
+    lastBuild: "29-05-2025"
 
 };

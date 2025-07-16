c93d586d0b60722ecdbdc50e0cbf424e77525fc6
IrfanTheDev <theirfan2020@gmail.com>
2025-07-16 11:49:28 +0530

    version update

--- src/constant.js
+++ src/constant.js
@@ -3,13 +3,17 @@ export default {
     // Variables to control the code and feature based on sapces,
     // So that even after merging code does not break or go into production without testing.
 
-    isLocal: true, //When running it locally.
+    isLocal: false, //When running it locally.
     isDev: false,  //For dev space only, also this should be true while deploying to Dev space
     isTest: true, // for test space only, this should be true while deploying to test space
     isProd: false, // for prod space only, and while deploying to the prod
 
-    version: "0.5.9",
-    lastBuild: "13-06-2025"
+    version: "0.6.0",
+    lastBuild: "20-06-2025"
+    // Approved by in OT and leave status too
+
+    // version: "0.5.9",
+    // lastBuild: "13-06-2025"
     // Approved By and Last modified at
     // RoleBand A visibility of meal and Transport
 

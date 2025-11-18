87d487fd84bd37e1b8d40aaf8cc826f09d7bea87
IrfanTheDev <theirfan2020@gmail.com>
2025-11-18 17:53:53 +0530

    version updated

--- src/constant.js
+++ src/constant.js
@@ -3,13 +3,18 @@ export default {
     // Variables to control the code and feature based on sapces,
     // So that even after merging code does not break or go into production without testing.
 
-    isLocal: true, //When running it locally.
+    isLocal: false, //When running it locally.
     isDev: false,  //For dev space only, also this should be true while deploying to Dev space
     isTest: true, // for test space only, this should be true while deploying to test space
     isProd: false, // for prod space only, and while deploying to the prod
 
-    version: "0.6.0",
-    lastBuild: "20-06-2025"
+    version: "0.6.1",
+    lastBuild: "18-11-2025"
+    // Meal and Transport column visibilty bug fixed
+
+
+    // version: "0.6.0",
+    // lastBuild: "20-06-2025"
     // Approved by in OT and leave status too
 
     // version: "0.5.9",
